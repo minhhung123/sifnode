a398b869349247de075ef42655be756b1b0ded34
Jure Žvikart <jure@sifchain.finance>
2022-03-23 18:10:13 +0100

    WIP

--- test/integration/src/peggy2/test_eth_transfers_grpc.py
+++ test/integration/src/peggy2/test_eth_transfers_grpc.py
@@ -24,8 +24,8 @@ def _test_eth_to_ceth_and_back_grpc(ctx, count, randomize=False):
     test_sif_account_initial_balance = ctx.get_sifchain_balance(test_sif_account)
 
     # Send from ethereum to sifchain by locking
-    amount_to_send_in_tx = 123456 * eth.GWEI
-    total_amount_to_send = amount_to_send_in_tx * count
+    amount_per_tx = 123456 * eth.GWEI
+    total_amount_to_send = amount_per_tx * count
     assert total_amount_to_send < fund_amount_eth
 
     ctx.bridge_bank_lock_eth(test_eth_account, test_sif_account, total_amount_to_send)
@@ -41,7 +41,7 @@ def _test_eth_to_ceth_and_back_grpc(ctx, count, randomize=False):
     # > sifnoded tx ethbridge burn
     # Reduce amount for cross-chain fee. The same formula is used inside this function.
     eth_balance_before = ctx.eth.get_eth_balance(test_eth_account)
-    amount_to_send = amount_to_send_in_tx - ctx.eth.cross_chain_fee_base * ctx.eth.cross_chain_burn_fee
+    amount_to_send = amount_per_tx - ctx.eth.cross_chain_fee_base * ctx.eth.cross_chain_burn_fee
 
     # chain_id, acccount_number and sequence are part of signature bytes and serve for replay protection.
     # chain_id and account_number do not change for the lifetime of chain, whereas sequence needs to be incremented for
@@ -57,21 +57,24 @@ def _test_eth_to_ceth_and_back_grpc(ctx, count, randomize=False):
     start_time = time.time()
     for i in range(count):
         # "generate_only" tells sifnode to print a transaction as JSON instead of signing and broadcasting it
-        tx = ctx.sifnode_client.send_from_sifchain_to_ethereum(test_sif_account, test_eth_account, amount_to_send,
+        tx = ctx.sifnode_client.send_from_sifchain_to_ethereum(test_sif_account, test_eth_account, amount_per_tx,
             ctx.ceth_symbol, generate_only=True)
         signed_tx = ctx.sifnode_client.sign_transaction(tx, test_sif_account, sequence=tx_sequence_no,
             account_number=account_number)
         encoded_tx = ctx.sifnode_client.encode_transaction(signed_tx)
         signed_encoded_txs.append(encoded_tx)
         tx_sequence_no += 1
-
     log.debug("Transaction generation speed: {:.2f}/s".format(count / (time.time() - start_time)))
+
+    sif_balance_before = ctx.get_sifchain_balance(test_sif_account)
+    eth_balance_before = ctx.eth.get_eth_balance(test_eth_account)
 
     rnd = random.Random(9999) if randomize else None
     log.debug("Broadcasting {} transactions{}...".format(count, " in random order" if rnd else ""))
     start_time = time.time()
     while signed_encoded_txs:
         next_tx_idx = rnd.randrange(len(signed_encoded_txs)) if rnd else 0
+        log.debug("Next index: {}".format(next_tx_idx))
         tx = signed_encoded_txs.pop(next_tx_idx)
         # result is a BroadcastTxResponse; result.tx_response is a TxResponse containing txhash etc.
         result = ctx.sifnode_client.broadcast_tx(tx)
@@ -82,18 +85,26 @@ def _test_eth_to_ceth_and_back_grpc(ctx, count, randomize=False):
         # new_eth_balance = ctx.wait_for_eth_balance_change(test_eth_account, eth_balance_before)
         new_eth_balance = ctx.eth.get_eth_balance(test_eth_account)
         balance_delta = new_eth_balance - eth_balance_before
-        log.debug("Balance difference: {} ({:.9f} / {})".format(balance_delta, balance_delta/amount_to_send_in_tx,
-            count * amount_to_send_in_tx - balance_delta))
+        still_to_go = count * amount_per_tx - balance_delta
+        log.debug("Balance difference: {} ({:.9f} / {})".format(balance_delta, balance_delta/amount_per_tx,
+            still_to_go))
+        if still_to_go == 0:
+            break
         time.sleep(3)
 
+    sif_balance_after = ctx.get_sifchain_balance(test_sif_account)
+    eth_balance_after = ctx.eth.get_eth_balance(test_eth_account)
 
-def _test_setup(ctx):
-    pass
-
+    # Change of test_sif_account per transaction: - 100000 rowan - (amount_per_tx + 1) ceth
+    # Change of test_Eth_account per transaction: amount_per_tx ETH
+    assert sif_balance_before["rowan"] - sif_balance_after["rowan"] == 100000 * count
+    assert sif_balance_before[ctx.ceth_symbol] - sif_balance_after[ctx.ceth_symbol] == (amount_per_tx + 1) * count
+    assert eth_balance_after - eth_balance_before = count * amount_per_tx
+    
 
 # Enable running directly, i.e. without pytest
 if __name__ == "__main__":
     basic_logging_setup()
     from siftool import test_utils
     ctx = test_utils.get_env_ctx()
-    _test_eth_to_ceth_and_back_grpc(ctx, 150, randomize=False)
+    _test_eth_to_ceth_and_back_grpc(ctx, 5)
